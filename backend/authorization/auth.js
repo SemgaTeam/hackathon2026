cbc0f392c485ec518f9e861ce6e9e5d3bfe8f37f
prorok3388 <mikhail_prikhodko_96@mail.ru>
2026-03-21 02:14:06 +0300

    ПИШЕМ АПИ

--- backend/authorization/auth.js
+++ backend/authorization/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const session = require("express-session");
 const { Client } = require("pg");
 const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
 
 const app = express();
 
@@ -22,12 +23,33 @@ app.use(
     },
   }),
 );
+const hashedPassword = async (password) => {
+  const saltRounds = 10;
+  const hashPassword = await bcrypt.hash(password, saltRounds);
+  return hashPassword;
+};
+
+app.post("/registr", async (req, res) => {
+  const { name, password } = req.body;
+  const pass = hashedPassword(password);
+  const query =
+    "INSERT INTO users (name, password) VALUES = ($1, $2) RETURNING *";
+  const values = [name, pass];
+  const result = await Client.query(query, values);
+  res.session.user = { id: result[0], name: result[1] };
+  res.status(201).send("User registr!");
+});
+app.post("/login", async (req, res) => {
+  const { name, password } = req.body;
+  const query = "SELECT";
+});
 
 const createCookie = async (req, res) => {
-    const sid = req.
-    const query = "INSERT INTO sessions (sid) VALEUS ($1) RETURNING *"
-    const values = []
-}
+  const sid = req.session.sid;
+  console.log;
+  const query = "INSERT INTO sessions (sid) VALEUS ($1) RETURNING *";
+  const values = [];
+};
 
 const sessionStore = {
   async get(req) {
