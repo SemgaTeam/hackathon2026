5ace404f456236e4ddbd9ac1bf56b4603a620c08
losion <losion445@gmail.com>
2026-03-21 02:08:15 +0300

    [add] Types for postgres and query manager for database queries

---
+++ backend/lib/databaseClient.ts
@@ -0,0 +1,32 @@
+import { QueryResult, QueryResultRow } from './../../frontend/node_modules/@types/pg/index.d';
+import { Pool, Query } from "pg";
+
+class Database {
+    private static instance: Pool;
+
+    private constructor() {}
+
+    public static getInstance(): Pool {
+        if (!Database.instance) {
+            Database.instance = new Pool({
+                host: process.env.DB_HOST,
+                user: process.env.DB_USER,
+                password: process.env.DB_PASSWORD,
+                database: process.env.DB_NAME,
+                port: Number(process.env.DB_PORT) || 5432,
+                max: 20,
+                idleTimeoutMillis: 30000,
+            });
+
+            Database.instance.on("error", (err) => {
+                console.error("Unexpected error on client", err);
+                process.exit(1);
+            });
+        }
+        return Database.instance;
+    }
+}
+
+export const query = <T extends QueryResultRow = any>(text: string, params?: any[]): Promise<QueryResult<T>> => {
+    return Database.getInstance().query<T>(text, params);
+};
