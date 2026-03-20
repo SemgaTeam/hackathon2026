3235669fee1256fcc00af28882e701f865086148
prorok3388 <mikhail_prikhodko_96@mail.ru>
2026-03-21 01:41:05 +0300

    authNotReady

--- backend/authorization/auth.js
+++ backend/authorization/auth.js
@@ -1,24 +1,44 @@
 const express = require("express");
 const session = require("express-session");
 const { Client } = require("pg");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 app.use(express.json());
-
-store = {
-  async get(sid) {
-    const query = "SELECT * FROM sessions WHERE sid = $1;";
-    const values = [sid];
-    const res = await Client.query(query, values);
-    return res;
-  },
-  set(sid, session) {},
-};
-
+//app.use(cookieParser(process.env.SECRET));
 app.use(
   session({
+    name: "connect.sid",
     secret: process.env.SECRET,
     resave: false,
+    saveUninitialized: false,
+
+    cookie: {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: process.env.SESSION_AT,
+    },
   }),
 );
+
+const createCookie = async (req, res) => {
+    const sid = req.
+    const query = "INSERT INTO sessions (sid) VALEUS ($1) RETURNING *"
+    const values = []
+}
+
+const sessionStore = {
+  async get(req) {
+    const { sid } = req.body;
+    const query = "SELECT * FROM sessions WHERE sid =$1";
+    const values = [sid];
+    const result = await Client.query(query, values);
+    return result;
+  },
+  async set(req) {
+    const { sid } = req.body;
+    const query = "INSERT INTO sessions (sid) VALUES = $1";
+  },
+};
