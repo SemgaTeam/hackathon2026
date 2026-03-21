a574d1262cebf147d87320619301c754ea404272
prorok3388 <mikhail_prikhodko_96@mail.ru>
2026-03-21 03:37:09 +0300

    кривая готова авторизация

--- backend/authorization/auth.js
+++ backend/authorization/auth.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const session = require("express-session");
 const { Client } = require("pg");
-const cookieParser = require("cookie-parser");
 const bcrypt = require("bcrypt");
+const dotenv = require("dotenv");
+require("dotenv").config();
 
 const app = express();
 
@@ -17,9 +18,9 @@ app.use(
 
     cookie: {
       httpOnly: true,
-      secure: true,
+      secure: false,
       sameSite: "strict",
-      maxAge: process.env.SESSION_AT,
+      maxAge: +process.env.SESSION_AT,
     },
   }),
 );
@@ -30,37 +31,48 @@ const hashedPassword = async (password) => {
 };
 
 app.post("/registr", async (req, res) => {
-  const { name, password } = req.body;
-  const pass = hashedPassword(password);
-  const query =
-    "INSERT INTO users (name, password) VALUES = ($1, $2) RETURNING *";
-  const values = [name, pass];
-  const result = await Client.query(query, values);
-  res.session.user = { id: result[0], name: result[1] };
-  res.status(201).send("User registr!");
+  try {
+    const { name, password } = req.body;
+    const pass = hashedPassword(password);
+    const query =
+      "INSERT INTO users (name, password) VALUES = ($1, $2) RETURNING *";
+    const values = [name, pass];
+    const result = await Client.query(query, values);
+    const User = result.rows[0];
+    res.session.user = { id: User.id, name: User.name };
+    res.status(201).send("User registr!");
+  } catch (error) {
+    res.status(401).json({ error });
+  }
 });
 app.post("/login", async (req, res) => {
-  const { name, password } = req.body;
-  const query = "SELECT";
-});
-
-const createCookie = async (req, res) => {
-  const sid = req.session.sid;
-  console.log;
-  const query = "INSERT INTO sessions (sid) VALEUS ($1) RETURNING *";
-  const values = [];
-};
-
-const sessionStore = {
-  async get(req) {
-    const { sid } = req.body;
-    const query = "SELECT * FROM sessions WHERE sid =$1";
-    const values = [sid];
+  try {
+    const { name, password } = req.body;
+    const query = "SELECT * FROM users WHERE name = $1";
+    const values = [name];
     const result = await Client.query(query, values);
-    return result;
-  },
-  async set(req) {
-    const { sid } = req.body;
-    const query = "INSERT INTO sessions (sid) VALUES = $1";
-  },
-};
+    const User = result.rows[0];
+    const isValid = await bcrypt.compare(password, User.password);
+    if (!isValid) {
+      res.status(401).send("Неверный пароль");
+    }
+    res.session.user = { id: User.id, name: User.name };
+    res.status(201).send("Вы авторизовались");
+  } catch (error) {
+    res.status(401).json({ error });
+  }
+});
+app.get("/authme", async (req, res) => {
+  try {
+    const query = "SELECT * FROM sessions WHERE sid = $1";
+    const values = [req.session.sid];
+    result = await Client.query(query, values);
+    const { sid } = req.session.sid;
+    if (result == sid) {
+      res.status(200).send("Авторизован");
+    }
+  } catch (error) {
+    res.status(401).send("Не авторизован");
+  }
+});
+app.listen(8000, console.log("http://localhost:8000"));
