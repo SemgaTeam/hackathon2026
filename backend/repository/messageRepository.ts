b2101414b8b654f3e0308613c6bd910d4f090f21
prorok3388 <mikhail_prikhodko_96@mail.ru>
2026-03-21 23:34:54 +0300

    messageRepository

--- backend/repository/messageRepository.ts
+++ backend/repository/messageRepository.ts
@@ -1,7 +1,31 @@
 import { UUID } from "node:crypto";
 import { Message } from "./message";
+import { QueryFunction } from "./users";
 
-export interface MessageRepository {
-    getAllByUserID(id: UUID): Promise<Message[]>;
-    Save(msg: Message): Promise<void>;
+export interface MessageInterface {
+  getAllByUserID(id: UUID): Promise<Message[]>;
+  Save(msg: Message): Promise<void>;
+}
+export class MessageRepository implements MessageInterface {
+  private readonly query: QueryFunction;
+
+  constructor(query: QueryFunction) {
+    this.query = query;
+  }
+  async Save(msg: Message): Promise<void> {
+    await this.query<Message>(
+      `INSERT INTO messages (user_id, sender_id, status, contents)`,
+    );
+  }
+  async getAllByUserID(id: UUID): Promise<Message[]> {
+    const { rows } = await this.query<Message[]>(
+      `SELECT * FROM messages WHERE id =$1 RETURNING *`,
+      [id],
+    );
+    const row = rows[0];
+    if (!row) {
+      throw new Error("error creating playlist");
+    }
+    return row;
+  }
 }
