63708c3f20479631da19dee981c9ec72a119954c
losion <losion445@gmail.com>
2026-03-21 14:40:00 +0300

    [fix] CORS

--- backend/main.ts
+++ backend/main.ts
@@ -18,7 +18,7 @@ const authController = new AuthController(userRepository);
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:5174',
     credentials: true,
     exposedHeaders: ['X-Total-Count']
 }));
